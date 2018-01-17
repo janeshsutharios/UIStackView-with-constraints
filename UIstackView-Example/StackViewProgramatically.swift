e3bdd743355cbc49178bc54547f9440d4bdf7483
AdityaBirlaHealth Common <abhcommon@AdityaBirlaHealths-Mac-mini.local>
2018-01-17 15:25:15 +0530

    Updated Swift 4

--- UIstackView-Example/StackViewProgramatically.swift
+++ UIstackView-Example/StackViewProgramatically.swift
@@ -1,60 +1,58 @@
-
 import UIKit
-
 class StackViewProgramatically: UIViewController {
     var propotionalStackView: UIStackView!
     let redView: UIView = {
-        let view = UIView()
-        view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
+        let view = UIView()//taking 42 % initially
+        view.frame = CGRect(x: 0, y: 0, width: 42 * UIScreen.main.bounds.width/100, height: UIScreen.main.bounds.height)
         view.backgroundColor = .red
         return view
     }()
+
     let greenView: UIView = {
-        let view = UIView()
-        view.frame = CGRect(x: UIScreen.main.bounds.width/3, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
+        let view = UIView()//taking 42* initially
+        view.frame = CGRect(x: 42 * UIScreen.main.bounds.width/100, y: 0, width: 25 * UIScreen.main.bounds.width/100, height: UIScreen.main.bounds.height)
         view.backgroundColor = .green
         return view
     }()
     let blueView: UIView = {
-        let view = UIView()
-        view.frame = CGRect(x: 2*UIScreen.main.bounds.width/3, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
+        let view = UIView()//taking 33*initially
+        view.frame = CGRect(x: 67 * UIScreen.main.bounds.width/100, y: 0, width: 33 * UIScreen.main.bounds.width/100, height: UIScreen.main.bounds.height)
         view.backgroundColor = .blue
         return view
     }()
 
+    //Changing UIView frame to supports landscape mode.
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-
-        print(UIScreen.main.bounds.width)
-        redView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
-        greenView.frame = CGRect(x: UIScreen.main.bounds.width/3, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
-        blueView.frame = CGRect(x: 2*UIScreen.main.bounds.width/3, y: 0, width: UIScreen.main.bounds.width/3, height: UIScreen.main.bounds.height)
+        super.viewWillTransition(to: size, with: coordinator)
+        DispatchQueue.main.async {
+            self.redView.frame = CGRect(x: 0, y: 0, width: 42 * self.widthPercent, height: self.screenHeight)
+            self.greenView.frame = CGRect(x: 42 * self.widthPercent, y: 0, width: 25 * self.widthPercent, height: self.screenHeight)
+            self.blueView.frame = CGRect(x: 67 * self.widthPercent, y: 0, width: 33 * self.widthPercent, height: self.screenHeight)
+        }
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Adding subViews to the stackView
         propotionalStackView = UIStackView()
-        view.addSubview(propotionalStackView)
-
-        propotionalStackView.addArrangedSubview(redView)
-        propotionalStackView.addArrangedSubview(greenView)
-        propotionalStackView.addArrangedSubview(blueView)
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        setStackView()
-    }
-    func setStackView() {
-        propotionalStackView?.translatesAutoresizingMaskIntoConstraints = false
-        let guide = self.view.safeAreaLayoutGuide
-        propotionalStackView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
-        propotionalStackView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
-        propotionalStackView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
-        propotionalStackView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
-
-        propotionalStackView.spacing = 1
+        propotionalStackView.addSubview(redView)
+        propotionalStackView.addSubview(greenView)
+        propotionalStackView.addSubview(blueView)
+        propotionalStackView.spacing = 0
         propotionalStackView.axis = .horizontal
         propotionalStackView.distribution = .fillProportionally
         propotionalStackView.alignment = .fill
-
+        view.addSubview(propotionalStackView)
     }
+}
+//MARK: UIscreen helper extension
+extension NSObject {
 
+    var widthPercent: CGFloat {
+        return UIScreen.main.bounds.width/100
+    }
+    
+    var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
 }
--- UIstackView-Example/ViewController.swift
+++ UIstackView-Example/ViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
 
-class ViewController: UIViewController {
 
-  
+class ViewController: UIViewController {
+    @IBOutlet weak var stackView: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //stackView.isLayoutMarginsRelativeArrangement = true
+
+    }
 }
 
